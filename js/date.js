5a5bec3d0f1c2a7114603496e6121eb022e38c43
Job Pulache <jobpulachecarrenojob837@gmail.com>
2025-01-05 15:15:48 -0500

    another way

--- js/date.js
+++ js/date.js
@@ -1,3 +1,9 @@
-//var date = new Date(2006, 2, 8) 
-var date = new Date("05/01/2025")
-alert(`the date today is ${date}`)
+var date = new Date(2006, 2, 8) 
+//var date = new Date("05/01/2025")
+
+//special functions
+var dia = date.getDate()
+var mes = date.getMonth()
+var anio = date.getFullYear()
+
+alert(`it's day: ${dia}, month: ${mes} and year is ${anio}`)
